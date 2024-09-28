78e9515e1fa003e7c39c1a6200aed0851632b675
“yukiwith5267” <yukiwith5267@icloud.com>
2024-09-28 23:59:24 +0900

    Implement DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,17 @@
-import 'package:app/core/secrets/app_secrets.dart';
 import 'package:app/core/theme/theme.dart';
-import 'package:app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:app/features/auth/domain/usecases/user_sign_in.dart';
-import 'package:app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:app/init_dependencies.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
+  await initDependencies();
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
-        create: (context) => AuthBloc(
-          userSignUp: UserSignUp(
-            authRepository: AuthRepositoryImpl(
-              remoteDataSource: AuthRemoteDataSourceImpl(
-                supabase: supabase.client,
-              ),
-            ),
-          ),
-          userSignIn: UserSignIn(
-            authRepository: AuthRepositoryImpl(
-              remoteDataSource: AuthRemoteDataSourceImpl(
-                supabase: supabase.client,
-              ),
-            ),
-          ),
-        ),
+        create: (context) => getIt<AuthBloc>(),
       ),
     ],
     child: const MyApp(),
